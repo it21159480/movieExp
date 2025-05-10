2a30ca624b7e3b28856d287885e32445b212e5ce
Mufeel Ishad <99629231+it21159480@users.noreply.github.com>
2025-05-10 21:12:19 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+import footerBG from '../assets/footer-bg.jpg'
 
 const Footer = () => {
   return (
-    <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
+    <footer 
+      className='text-center text-neutral-400 py-2 h-72' 
+      style={{ backgroundImage: `url(${footerBG})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
         <div className='flex items-center justify-center gap-4'>
-          <Link to="/" >About</Link>
-          <Link to="/">Contact</Link>
+          <Link to={"/"}>
+            <img
+                src={logo}
+                alt='logo'
+                width={180} 
+            />
+          </Link>
         </div>
-        <p className='text-sm'>Created By Dynamic Coding with Amit</p>
+<p className='text-sm'>© 2025 All Rights Reserved</p>
     </footer>
   )
 }
